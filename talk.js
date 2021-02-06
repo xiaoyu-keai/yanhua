a156be3da765d781a1deb0af404d9d1390a5089b
1226816173 <1226816173@qq.com>
2021-02-06 13:04:13 +0800

    a

--- talk.js
+++ talk.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#yes').click(function(event) {
-        modal('我就知道小姐姐您一定会愿意的。(^_^)', function() {
+        modal('我就知道小胖子一定会愿意的。(^_^)', function() {
             $('.page_one').addClass('hide');
             $('.page_two').removeClass('hide');
             // typeWrite();
